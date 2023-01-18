71186409572f897ee429bc988250c26a766600af
cristopheridlc <70478502+cristopheridlc@users.noreply.github.com>
2023-01-17 19:25:06 -0500

    Initial commit

---
+++ mp1.js
@@ -0,0 +1,14 @@
+'use strict'
+// Submission Information
+// [Fill in assignment information below this line]
+// Name: Cristopher De La Cruz
+// Assignment/Purpose: New Program Structure
+// Due Date: 1/22/2023
+// Version: 1
+
+function DeLaCruzCristopherMP1() {
+    // The program body for MP2-MP6 goes here
+    return 0;
+}
+
+DeLaCruzCristopherMP1();
