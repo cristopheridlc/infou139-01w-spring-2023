d34e57681d7e6cabff6ffb1e2041d7a9ed0d00be
cristopheridlc <70478502+cristopheridlc@users.noreply.github.com>
2023-01-24 16:32:28 -0500

    mp2 assignment

---
+++ mp2.js
@@ -0,0 +1,31 @@
+'use strict'
+// Submission Information
+// [Fill in assignment information below this line]
+// Name: Cristopher De La Cruz
+// Assignment/Purpose: Mini Program 2
+// Due Date: 1/29/2023
+// Version: 1
+
+function DeLaCruzCristopherMP2() {
+        // The program body for MP2-MP6 goes here
+        // VARIABLES DECLARATION SECTION
+        // Input
+            var strCDFullName;
+            var numCDAge;
+            var blnCDGender; //true is female
+            var dblCDPi;
+        // Processing
+        // Output
+        // VARIABLE DATA ASSIGNMENTS
+        // Assign the values to the variables created in the DECLARATION SECTION
+        // Input
+            strCDFullName = "Cristopher De La Cruz"
+            numCDAge = 23
+            blnCDGender = false
+            dblCDPi = 3.14
+        // Processing
+        // Output
+    return 0;
+}
+
+DeLaCruzCristopherMP2();
